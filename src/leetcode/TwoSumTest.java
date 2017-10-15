a70cd132498bc65d5efdae2b4ee74223f8b50601
alma-dell\sasha <godin_a_e@mail.ru>
2017-10-15 20:02:50 +0300

    -- changed formatting to be in 80 symbols mostly. -- segregated multiple run to "testMany"

--- src/leetcode/TwoSumTest.java
+++ src/leetcode/TwoSumTest.java
@@ -10,6 +10,25 @@ import org.junit.runner.Description;
 
 public class TwoSumTest {
 
+    @Test
+    public void test() {
+        TwoSum theTwoSum = new TwoSum();
+        Assert.assertNull(theTwoSum.twoSum(new int[] {},  0));
+        Assert.assertNull(theTwoSum.twoSum(new int[] {1}, 1));
+        Assert.assertNull(theTwoSum.twoSum(new int[] {3, 2},    8));
+        Assert.assertNull(theTwoSum.twoSum(new int[] {3, 2, 4}, 8));
+        Assert.assertArrayEquals(new int[] {0, 1},
+                theTwoSum.twoSum(new int[] {3, 2},    5));
+        Assert.assertArrayEquals(new int[] {0, 1},
+                theTwoSum.twoSum(new int[] {3, 2, 4}, 5));
+        Assert.assertArrayEquals(new int[] {1, 2},
+                theTwoSum.twoSum(new int[] {3, 2, 4}, 6));
+        Assert.assertArrayEquals(new int[] {0, 2},
+                theTwoSum.twoSum(new int[] {3, 2, 4}, 7));
+        Assert.assertArrayEquals(new int[] {998, 999},
+                theTwoSum.twoSum(IntStream.rangeClosed(1, 1000).toArray(), 1999));
+    }
+
     @Rule
     public final Stopwatch stopwatch = new Stopwatch() {
         @Override
@@ -19,24 +38,8 @@ public class TwoSumTest {
         }
     };
 
-    @Test
-    public void test() {
-        IntStream.rangeClosed(1, 10000000).forEach($ -> {
-            TwoSum theTwoSum = new TwoSum();
-            Assert.assertNull(theTwoSum.twoSum(new int[] {},  0));
-            Assert.assertNull(theTwoSum.twoSum(new int[] {1}, 1));
-            Assert.assertNull(theTwoSum.twoSum(new int[] {3, 2},    8));
-            Assert.assertNull(theTwoSum.twoSum(new int[] {3, 2, 4}, 8));
-            Assert.assertArrayEquals(new int[] {0, 1},
-                    theTwoSum.twoSum(new int[] {3, 2},    5));
-            Assert.assertArrayEquals(new int[] {0, 1},
-                    theTwoSum.twoSum(new int[] {3, 2, 4}, 5));
-            Assert.assertArrayEquals(new int[] {1, 2},
-                    theTwoSum.twoSum(new int[] {3, 2, 4}, 6));
-            Assert.assertArrayEquals(new int[] {0, 2},
-                    theTwoSum.twoSum(new int[] {3, 2, 4}, 7));
-            Assert.assertArrayEquals(new int[] {8, 9},
-                    theTwoSum.twoSum(IntStream.rangeClosed(1, 10).toArray(), 19));
-        });
+    //@Test
+    public void testMany() {
+        IntStream.rangeClosed(1, 1000).forEach($ -> test());
     }
 }
